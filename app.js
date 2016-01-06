4eeabd484789e29c0c7794da557133b5418721aa
dhoffmann <dan@danhoffmann.com>
2016-01-06 14:07:59 -0800

    set port

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.get('/:url', function (req, res) {
   }
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-});
+app.set('port', (process.env.PORT || 3000));
+
+var server = app.listen(app.get('port'), function () {});
