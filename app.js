7a9014b7408fe4c018e657983e11d3abdd44fbcb
dhoffmann <dan@danhoffmann.com>
2016-01-07 15:06:49 -0800

    scrape with just cheerio and request

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
-var ogs = require('open-graph-scraper');
-
+var request = require('request');
+request = request.defaults({ jar: request.jar() });
+var cheerio = require ('cheerio');
 app.options('/:url', cors());
+
 app.get('/:url', cors(), function (req, res) {
   var url = decodeURIComponent(req.params.url);
   if (url) {
-    var options = {'url':url, 'timeout':15000};
-    ogs(options, function(err, results) {
-      if (results.success) {
-        res.json(results);
+    request(url, function (error, response, body) {
+      if (!error) {
+        var $ = cheerio.load(body);
+        var tags = [];
+        $('meta[property*="og:"], meta[property*="twitter:"]').each(function(i, el){
+          tags[i] = {
+            property: $(this).attr('property'),
+            content: $(this).attr('content')
+          }
+        });
+        res.json(tags);
       } else {
+        console.log(error);
         res.sendStatus(404);
       }
     });
